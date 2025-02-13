92c276ebb889ecf519cef34121fa5283de724965
sitaratoon <ajay393798@gmail.com>
2025-02-13 15:02:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,6 @@ file_auto_delete = humanize.naturaldelta(subaru)
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    
     id = message.from_user.id
     if not await present_user(id):
         try:
